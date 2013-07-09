4d94291ba1413de2f89e0b6974c4e0fda998eb2c
Malgorzata Janczarska <malgorzata.tomczyk@pl.ibm.com>
2013-07-09 13:39:09 +0200

    Bug 401990 - [Backport][Edit] "Copy Current from Right to Left" doesn't work when triggered after Undo

--- bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/internal/merge/DocumentMerger.java
+++ bundles/org.eclipse.compare/plugins/org.eclipse.compare/compare/org/eclipse/compare/internal/merge/DocumentMerger.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2012 IBM Corporation and others.
+ * Copyright (c) 2007, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1245,7 +1245,7 @@ public class DocumentMerger {
 	 */
 	public boolean copy(Diff diff, boolean leftToRight) {
 		
-		if (diff != null && !diff.isResolved()) {
+		if (diff != null) {
 			Position fromPos= null;
 			Position toPos= null;
 			IDocument fromDoc= null;
